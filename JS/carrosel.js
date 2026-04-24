a1d96b5ea7a1daeb0dcbf809fb772cd45c60933a
Italo Luan de Almeida <contato.almeidagrupo@gmail.com>
2026-04-24 14:53:34 -0400

    last task

--- JS/carrosel.js
+++ JS/carrosel.js
@@ -3,13 +3,13 @@ const total = 3;
 
 function update(){
     const carrosel_principal = document.getElementById("carrosel-principal");
-    carrosel_principal.style.transform = `trasnlateX(-${index * 100}%)`;
+    carrosel_principal.style.transform = `translateX(-${index * 100}%)`;
 }
 function next(){
     index = (index + 1) % total;
     update();
 }
 function prev(){
-    index = (index - 1) % total;
+    index = (index - 1 + total) % total;
     update();
 }
